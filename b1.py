ca3005d0066f39ed3900e2a52b8e52c416c2463a
Ankita Mishra <ankitamishra2617@gmail.com>
2025-12-23 21:56:00 +0530

    Copying a binary file

--- b1.py
+++ b1.py
@@ -2,6 +2,6 @@ f=open(r"C:\Users\ankita\Desktop\Passport photo.jpeg","rb")
 f1=open(r"C:\Users\ankita\Desktop\photo.jpeg","wb")
 for i in f:
     f1.write(i)
-print("Photo created")
+print("Photo created Succesfully")
 f.close()
 f1.close()
